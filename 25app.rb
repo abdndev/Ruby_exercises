6a66ba4266730c00ffd94bb5f8b987964a793597
ABDNdev <igor@abidin.ru>
2019-10-01 20:26:51 +0300

    execute simple query

--- 25app.rb
+++ 25app.rb
@@ -1,9 +1,10 @@
 require 'sqlite3'
 
-db = SQLite3::Database.new 'mydatabase.db'
+db = SQLite3::Database.new 'test.sqlite'
 
-db.execute "CREATE TABLE "Motos" ("Id" INTEGER PRIMARY KEY AUTOINCREMENT, "Name" VARCHAR, "Price" INTEGER)" 
-
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 7777)"
+db.execute "INSERT INTO Cars (Name, Price) VALUES ('Mazda', 9977)"
+db.execute "INSERT INTO Cars (Name, Price) VALUES ('Pontiak', 12657)"
+db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 8888)"
+db.execute "INSERT INTO Cars (Name, Price) VALUES ('Lada', 5576)"
 
 db.close
