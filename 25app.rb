2dce624c491712d122a6de6819c7a95e8c119577
ABDNdev <igor@abidin.ru>
2019-10-01 20:38:15 +0300

    reading from db

--- 25app.rb
+++ 25app.rb
@@ -2,9 +2,14 @@ require 'sqlite3'
 
 db = SQLite3::Database.new 'test.sqlite'
 
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Mazda', 9977)"
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Pontiak', 12657)"
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 8888)"
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Lada', 5576)"
+#db.execute "INSERT INTO Cars (Name, Price) VALUES ('Mazda', 9977)"
+#db.execute "INSERT INTO Cars (Name, Price) VALUES ('Pontiak', 12657)"
+#db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 8888)"
+#db.execute "INSERT INTO Cars (Name, Price) VALUES ('Lada', 5576)"
+
+db.execute "SELECT * FROM Cars" do |car|
+	puts car
+	puts"==="
+end
 
 db.close
