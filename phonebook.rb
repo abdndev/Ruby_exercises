b4605104239a4fe035e021c843550f64d50de0f0
ABDNdev <igor@abidin.ru>
2019-10-02 17:09:22 +0400

    simple phonebook

---
+++ phonebook.rb
@@ -0,0 +1,22 @@
+
+hh = {}
+
+while true
+print "Enter name (Enter to stop): "
+name = gets.strip.capitalize
+
+print "Enter phone number: "
+phone = gets.to_i
+
+hh["#{name}"] = phone 
+
+#=begin
+	if name == ""
+	#elsif phone == ""
+	puts hh
+        exit
+	end
+#=end
+
+#puts hh
+end
