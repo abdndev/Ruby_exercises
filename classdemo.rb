d70fc119c241bb86e13e434d53ca5dc8f8f18fb9
ABDNdev <igor@abidin.ru>
2019-10-03 14:16:52 +0300

    non-modifided classdemo file

---
+++ classdemo.rb
@@ -0,0 +1,18 @@
+# encoding: cp866
+class Animal
+	def run
+		puts "I'm running!"
+	end
+	def run_rus
+		puts "Я уже бегу!"
+	end
+
+end
+
+a = Animal.new
+a.run
+puts a
+
+b = Animal.new
+b.run_rus
+puts b
