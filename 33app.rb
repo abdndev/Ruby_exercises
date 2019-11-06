d85af1fe28c13b95c2c3a4423e987516cccc1d81
ABDNdev <igor@abidin.ru>
2019-11-06 16:25:56 +0300

    Modify program

--- 33app.rb
+++ 33app.rb
@@ -1,10 +1,20 @@
-print "Enter product id: "
-id = gets.chomp
-
-print "Enter amount (how much items you want to order): " 
-n = gets.chomp.to_i
-
 hh = {}
-hh[id] = n
 
-puts hh.inspect
+
+
+loop do
+
+	print 'Enter product id: '
+	id = gets.chomp
+
+	print 'Enter amount (how much items you want to order): ' 
+	n = gets.chomp.to_i
+
+	x = hh[id].to_i  # read hash value (0 if it's not exist)
+	x = x + n        # increase by n
+	hh[id] = x       # set hash value
+
+	puts hh.inspect
+	puts '====================================='
+
+end
