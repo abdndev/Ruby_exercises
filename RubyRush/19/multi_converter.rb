d4701bd8132281697a851254c66c2a3451ae2162
ABDNdev <igor@abidin.ru>
2019-12-05 20:22:14 +0300

    Add comment

--- RubyRush/19/multi_converter.rb
+++ RubyRush/19/multi_converter.rb
@@ -12,7 +12,7 @@ puts
 if currency == '1'
   print 'Сколько у вас рублей?  '
     qty_rub = gets.chomp.to_f
-    usd_value = (qty_rub / usd_rate).round(2)
+    usd_value = (qty_rub / usd_rate).round(2)    # метод .round(2) позволяет отсечь лишние знаки после запятой у вещественного числа
     puts
   puts 'Ваши запасы на сегодня равны: ' + '$' + usd_value.to_s
 else
