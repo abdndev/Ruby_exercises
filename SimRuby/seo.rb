808c5a8866e5bf797d02548d61a569305302c2fd
ABDNdev <igor@abidin.ru>
2020-06-25 18:41:36 +0300

    Modify file

--- SimRuby/seo.rb
+++ SimRuby/seo.rb
@@ -1,5 +1,5 @@
 module Seo
-  attr_accessor :meta_title, :meta_discription, :meta_keywords
+  attr_accessor :meta_title, :meta_description, :meta_keywords
 
   module ClassMethods
     def title(name)
