af831c10d4ae56150ab63595bc474b3c5adec38c
ABDNdev <igor@abidin.ru>
2020-06-26 17:47:24 +0300

    Class mixin is prohibited

---
+++ SimRuby/class_as_module.rb
@@ -0,0 +1,12 @@
+class Hello
+  def say(str)
+    "Hello, #{str}!"
+  end
+end
+
+class Greet
+  include Hello # wrong argument type Class (expected Module) (TypeError)
+end
+
+obj = Greet.new
+obj.extend Hello # wrong argument type Class (expected Module) (TypeError)
