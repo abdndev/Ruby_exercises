c9fd94501b285b439f028f7a2a940106d30bea41
ABDNdev <igor@abidin.ru>
2020-08-13 13:12:44 +0300

    Deleting undefined elements

---
+++ SimRuby/compact.rb
@@ -0,0 +1,7 @@
+arr = [1, 2, 3]
+arr[7] = 4
+p arr              # [1, 2, 3, nil, nil, nil, nil, 4]
+p arr.compact      # [1, 2, 3, 4]
+p arr              # [1, 2, 3, nil, nil, nil, nil, 4]
+p arr.compact!     # [1, 2, 3, 4]
+p arr              # [1, 2, 3, 4]
