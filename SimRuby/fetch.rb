1d9c735ae48e8c403387cf56c30616c221763731
ABDNdev <igor@abidin.ru>
2020-08-31 18:20:21 +0300

    Using method "fetch"

--- SimRuby/fetch.rb
+++ SimRuby/fetch.rb
@@ -1,5 +1,4 @@
-arr = [1, 2, 3, 4, 5]
+h = { first: 1, second: 2 }
 
-puts arr.fetch(2)   # 3
-puts arr.fetch(-2)  # 4
-puts arr.fetch(10)  # index 10 outside of array bounds: -5...5
+puts h.fetch(:first)   # 1
+puts h.fetch(:third)   # `fetch': key not found: :third (KeyError)
