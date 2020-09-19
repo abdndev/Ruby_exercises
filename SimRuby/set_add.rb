ff01a3dc3e2bb880f2c0ca07ffdaedd753a9bfa7
ABDNdev <igor@abidin.ru>
2020-09-19 15:54:25 +0300

    Adding elements to "Set"

---
+++ SimRuby/set_add.rb
@@ -0,0 +1,15 @@
+require 'set'
+
+workday = Set.new
+
+p workday # #<Set: {}>
+
+workday << 'monday'
+workday << 'tuesday'
+workday << 'wednesday'
+workday << 'thursday'
+workday << 'friday'
+
+p workday
+
+# #<Set: {"monday", "tuesday", "wednesday", "thursday", "friday"}>
