adf4fb5fcfe6e9783805fbc2ae1f3edd8fe8217b
ABDNdev <igor@abidin.ru>
2020-10-03 06:33:26 +0300

    Add file

---
+++ SimRuby/rand_exception_rescue.rb
@@ -0,0 +1,12 @@
+require_relative 'rand_exception'
+
+begin
+  10.times.map do |i|
+    p Ticket.new date: Time.new(2019, 5, 10, 10, i)
+  end
+
+  puts tickets.size
+rescue
+rescue RandException => e
+  puts "Возникла ошибка RandException: #{e.message}"
+end
