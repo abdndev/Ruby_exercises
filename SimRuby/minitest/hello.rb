e52961a59d7cba501637e712a1c8d9bcd8de2b07
ABDNdev <igor@abidin.ru>
2021-01-03 17:00:42 +0300

    Error message

--- SimRuby/minitest/hello.rb
+++ SimRuby/minitest/hello.rb
@@ -1,4 +1,18 @@
+require 'minitest/autorun'
+
 class Hello
   def say(str)
   end
 end
+
+class TestHello < MiniTest::Unit::TestCase
+  def setup
+    @object = Hello.new
+  end
+
+  def test_that_hello_return_a_string
+    assert_instance_of String, @obj.say('test')
+  end
+end
+
+
