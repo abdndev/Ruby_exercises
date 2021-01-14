f63acc68a9ec08d6d69532f3b5ee1912c5558c1c
ABDNdev <igor@abidin.ru>
2021-01-14 05:41:09 +0300

    Testing changes of key

--- SimRuby/rspec/spec/let_spec.rb
+++ SimRuby/rspec/spec/let_spec.rb
@@ -9,5 +9,15 @@ RSpec.describe 'Объект' do
     it 'должен содержать корректный ключ' do
       expect(object.key).to eq 'ключ'
     end
+
+    context 'с измененным значением' do
+      before(:each) do
+        object.key = 'новый ключ'
+      end
+
+      it 'должен содержать новый ключ' do
+        expect(object.key).to eq 'новый ключ'
+      end
+    end
   end
 end
