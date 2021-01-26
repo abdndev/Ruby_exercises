98b0cb0be4d854bf1c539a53f370d93346a00e66
ABDNdev <igor@abidin.ru>
2021-01-26 16:29:05 +0300

    Adding second & third clients data requerers

--- PushkinBook/clients.rb
+++ PushkinBook/clients.rb
@@ -1,4 +1,5 @@
 puts 'This program will output our three clients data'
+puts
 print 'First client, please enter your name: '
 cname1 = gets.strip.capitalize
 print 'First client, please enter your date of birth: '
@@ -7,6 +8,27 @@ print 'First client, please enter your place of birth: '
 cplace1 = gets.strip.capitalize
 print 'First client, please enter your phone number: '
 cphone1 = gets.strip.to_s
+puts
+
+print 'First client, please enter your name: '
+cname2 = gets.strip.capitalize
+print 'First client, please enter your date of birth: '
+cdate2 = gets.strip
+print 'First client, please enter your place of birth: '
+cplace2 = gets.strip.capitalize
+print 'First client, please enter your phone number: '
+cphone2 = gets.strip.to_s
+puts
+
+print 'First client, please enter your name: '
+cname3 = gets.strip.capitalize
+print 'First client, please enter your date of birth: '
+cdate3 = gets.strip
+print 'First client, please enter your place of birth: '
+cplace3 = gets.strip.capitalize
+print 'First client, please enter your phone number: '
+cphone3 = gets.strip.to_s
+puts
 
 puts '======================================='
 puts "name: #{cname1}"
