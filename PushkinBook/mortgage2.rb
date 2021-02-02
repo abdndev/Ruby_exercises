542224dcd9cb78ccf9336cdf7e693ed897f9145c
ABDNdev <igor@abidin.ru>
2021-02-02 09:35:33 +0300

    Add the total percent sum of mortgage

--- PushkinBook/mortgage2.rb
+++ PushkinBook/mortgage2.rb
@@ -1,9 +1,13 @@
 sum = 500000
+total = 0
 
 30.times do |n|
   sum = sum - 16666 
   perc = sum * 0.04
   perc = perc.to_i
   annual_perc = perc /(30 - n)
+  total = total + annual_perc
   puts "Год #{n + 1}, проценты за кредит составляют: $#{annual_perc}"
 end
+puts
+puts "Общая сумма процентов: $#{total}"
