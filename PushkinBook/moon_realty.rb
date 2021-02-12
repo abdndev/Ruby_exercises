f1aab7aac1f8052e022771463c0dad37566b1fcc
ABDNdev <igor@abidin.ru>
2021-02-12 18:01:40 +0300

    Add & modify file

--- PushkinBook/moon_realty.rb
+++ PushkinBook/moon_realty.rb
@@ -7,19 +7,21 @@ print 'Enter the length: '
 length = gets.to_i
 print 'Enter the width: '
 width = gets.to_i
+puts
 
 square = length * width
+puts "The square of your moon real estate is: #{square}"
 
 if square < 50
   puts "The cost of your real estate is: $1000"
 end
 
-#if 50 <= square < 100
 if square >= 50 && square < 100
   puts "The cost of your real estate is: $1500"
 end
 
 if square >= 100
-  puts "The cost of your real estate is: $#{square * 25}"
+  puts "The cost of your real estate is: " + "$" + (square * 25).to_s
+  #puts "The cost of your real estate is: $#{square * 25}"
 end
 
