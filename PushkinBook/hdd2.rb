c28e789eb8e1f59cdd9a33d8f203ef08850ea7bc
ABDNdev <igor@abidin.ru>
2021-02-20 18:45:39 +0300

    Modify file

--- PushkinBook/hdd2.rb
+++ PushkinBook/hdd2.rb
@@ -1,12 +1,9 @@
 sym = %w[\\ | / -]
-#print 'Formatting hard drive'
-100_000.times { |i| print '@x' }
 
 loop do
   sym.each do |s|
     x = "\r#{s}"
     print x 
-    #print "#{s}\r" 
     #sleep rand(0.05..0.5)
     sleep 0.06
   end
