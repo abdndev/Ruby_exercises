87cce1a5804200061f284089f8f15b56897debb7
ABDNdev <igor@abidin.ru>
2021-03-23 18:47:03 +0300

    Modify file

--- PushkinBook/robots1.rb
+++ PushkinBook/robots1.rb
@@ -2,7 +2,26 @@ puts 'Robot battle game'
 
 arr1 = Array.new(10, 1)
 arr2 = Array.new(10, 1)
+p arr1
+loop do
+    puts 'Стреляет первая команда.'
+    i = rand(0..9)
+    if arr1[i] == 1
+        arr1[i] = 0
+        puts "Робот по индексу #{i} убит"
+    else
+        puts 'Промазали!'
+    end
+    sleep 1
 
-i = rand(0..9)
-
-
+    puts 'Стреляет вторая команда.'
+    i = rand(0..9)
+    if arr2[i] == 1
+        arr2[i] = 0
+        puts "Робот по индексу #{i} убит"
+    else
+        puts 'Промазали!'
+    end
+    p arr1
+    sleep 1
+end
