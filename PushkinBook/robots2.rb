f0988ca2ae8d813651ae5f5861d886ca7935afd0
ABDNdev <igor@abidin.ru>
2021-03-29 06:11:37 +0300

    Create new file

---
+++ PushkinBook/robots2.rb
@@ -0,0 +1,62 @@
+puts 'Игра "Битва Роботов"'
+puts
+puts 'В каждой команде по десять роботов'
+
+arr1 = Array.new(10, 1)
+arr2 = Array.new(10, 1)
+
+puts "Команда 1: #{arr1}"
+puts "Команда 2: #{arr2}"
+puts
+
+loop do
+    puts 'Стреляет первая команда.'
+    i = rand(0..9)
+    if arr1[i] == 1
+        arr1[i] = 0
+        puts "Чужой робот по индексу #{i} убит"
+    else
+        puts "Промазали по индексу #{i}"
+    end
+    p arr1
+
+    x = 0
+    arr1.each do |element|
+        if element == 1
+            x += 1
+        end
+    end
+    puts "Во второй команде осталось роботов: #{x}"
+
+    if x == 0
+        puts "Выиграла первая команда! Игра окончена!"
+        exit
+    end
+    sleep 1
+    puts
+
+    puts 'Стреляет вторая команда.'
+    i = rand(0..9)
+    if arr2[i] == 1
+        arr2[i] = 0
+        puts "Чужой робот по индексу #{i} убит"
+    else
+        puts "Промазали по индексу #{i}"
+    end
+    p arr2
+
+    x = 0
+    arr2.each do |element|
+        if element == 1
+            x += 1
+        end
+    end
+    puts "В первой команде осталось роботов: #{x}"
+
+    if x == 0
+        puts "Выиграла вторая команда! Игра окончена!"
+        exit
+    end
+    sleep 1
+    puts
+end
