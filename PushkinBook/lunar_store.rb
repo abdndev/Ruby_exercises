62a3dd0914052538f47dda55ac92eb49c72fe5f7
ABDNdev <igor@abidin.ru>
2021-04-18 05:45:02 +0300

    Modify file

---
+++ PushkinBook/lunar_store.rb
@@ -0,0 +1,12 @@
+puts 'Hello! You are in Lunar-Store'
+puts
+puts 'Please Enter quantity of balls, which you want to buy'
+puts
+
+puts 'Soccer balls: '
+sb = gets.strip
+puts 'Tennis balls: '
+tb = gets.strip
+puts 'Golf balls: '
+gb = gets.strip
+
