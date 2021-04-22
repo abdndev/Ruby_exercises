bfda7939fe7d27d9660f2fc490373600f00417e1
ABDNdev <igor@abidin.ru>
2021-04-22 05:23:22 +0300

    Modify file

---
+++ PushkinBook/total_weight.rb
@@ -0,0 +1,11 @@
+def total_weight(options={})
+    a = options[:soccer_ball_count] || 0
+    b = options[:tennis_ball_count] || 0
+    c = options[:golf_ball_count] || 0
+    puts a 
+    puts b 
+    puts c  
+    (a * 410) + (b * 58) + (c * 45) + 29  # 29 - вес коробки
+  end
+
+  x = total_weight(soccer_ball_count: 3, tennis_ball_count: 2, golf_ball_count: 1)
