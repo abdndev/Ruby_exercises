a80f151d399e421454b1612b7dc2abac8779794f
ABDNdev <igor@abidin.ru>
2021-04-25 10:57:15 +0300

    Modify file

--- PushkinBook/samples.rb
+++ PushkinBook/samples.rb
@@ -51,3 +51,20 @@ def total_weight(options={})
   end
 
   x = total_weight(soccer_ball_count: 3, tennis_ball_count: 2, golf_ball_count: 1)
+--------------------------------------------------------------------------------
+
+# если в хеше options значение не указано (nil), то переменной будет присвоено значение "0", и ошибки не будет
+
+def total_weight(options={})
+    a = options[:soccer_ball_count] || 0
+    b = options[:tennis_ball_count] || 0
+    c = options[:golf_ball_count] || 0
+    puts a 
+    puts b 
+    puts c  
+    # (a * 410) + (b * 58) + (c * 45) + 29  # 29 - вес коробки
+  end
+
+  x = total_weight(soccer_ball_count: 3, tennis_ball_count: 2, golf_ball_count: 1)
+  puts x
+  
