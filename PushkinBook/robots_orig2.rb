d2dd0fd932be277c3ee9b31b6215783761652900
ABDNdev <igor@abidin.ru>
2021-04-29 15:30:13 +0300

    Modify file

--- PushkinBook/robots_orig2.rb
+++ PushkinBook/robots_orig2.rb
@@ -30,7 +30,10 @@ end
 
 def damage(arr)
   d = rand(1..100)
-  if arr[@i] > 0
+  if arr[@i] == 0
+    d == 0
+    #arr[@i] = arr[@i] - d
+  elsif arr[@i] > 0
     arr[@i] = arr[@i] - d
     puts "Робот по индексу #{@i} получил ущерб #{d} единиц"
   elsif (arr[@i] - d) < 0 || d == arr[@i]
