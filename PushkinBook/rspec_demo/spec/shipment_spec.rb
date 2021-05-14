593af783f1e507c84b61e0a1648f0ef1d7be7443
ABDNdev <igor@abidin.ru>
2021-05-14 17:31:26 +0300

    Modify file

--- PushkinBook/rspec_demo/spec/shipment_spec.rb
+++ PushkinBook/rspec_demo/spec/shipment_spec.rb
@@ -1,8 +1,8 @@
-require './lib/shipment'
-
-describe Shipment do
-  it 'should work without options' do
-    expect(Shipment.total_weight).to eq(29)
+require './lib/shipment'                        # подключаем юнит
+                                                # специальный синтаксис, который дословно говорит:
+describe Shipment do                            # "описываем Shipment (отправление)"
+  it 'should work without options' do           # специальный синтаксис, который дословно говорит: "это должно работать без опций". То что в кавычках - это строка, мы сами её пишем, слово "it" служебное.
+    expect(Shipment.total_weight).to eq(29)     # ожидаем, что общий вес отправления будет равен 29 (eq от англ. "equal")
   end
 end
 
