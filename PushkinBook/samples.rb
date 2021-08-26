9f95dc767789b6206df27f450fa2af80d688774c
ABDNdev <igor@abidin.ru>
2021-08-26 17:37:11 +0300

    Add sample

--- PushkinBook/samples.rb
+++ PushkinBook/samples.rb
@@ -1626,3 +1626,32 @@ end
 # Мы добавили еще одну опеарацию, которая для массивов не определена; метод peek возвращает элемент,
 # находящийся на вершине стека, не выталкивая его.
 
+# Более строгая реализация очереди. Если вы хотите защититься от некорректного доступа к структуре данных,
+# рекомендуем поступать аналогично.
+class Queue
+
+  def initialize
+    @store = []
+  end
+
+  def enqueue(x)
+    @store << x
+  end
+
+  def dequeue
+    @store.shift
+  end
+
+  def peek
+    @store.first
+  end
+
+  def length
+    @store.length
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+end
