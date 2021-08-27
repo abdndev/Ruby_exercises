a17ad0f3a76834332e6067bd5ad2621b1dafc245
ABDNdev <igor@abidin.ru>
2021-08-27 12:45:35 +0300

    Add new sample

--- PushkinBook/samples.rb
+++ PushkinBook/samples.rb
@@ -1655,3 +1655,22 @@ class Queue
   end
 
 end
+
+# Решение задачи "Ханойская башня" с помощью стека (с тремя дисками)
+def towers(list)
+  while !list.empty?
+    n, src, dst, aux = list.pop
+    if n == 1
+      puts "Перемещаем диск c #{src} на #{dst}"
+    else
+      list.push [n-1, aux, dst, src]
+      list.push [1, src, dst, aux]
+      list.push [n-1, src, aux, dst]
+    end
+  end
+end
+
+list = []
+list.push([3, "a", "c", "b"])
+
+towers(list)
