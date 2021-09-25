4dc2ec2f58c1d09f8e8665858d539453202632ec
ABDNdev <igor@abidin.ru>
2021-09-25 20:07:56 +0300

    Modifyfile

--- PushkinBook/selectee.rb
+++ PushkinBook/selectee.rb
@@ -1,9 +1,12 @@
 
 loop do
 
+dq = rand(0..1)                  # Selectee DQ status
 selectee_waits = rand(1..15)     # Selectee is waiting (months)
 
-if selectee_waits > 12
+if dq == 0
+  puts "Sasay! Just Sasay!"
+elsif selectee_waits > 12
   puts "Your visa approved!"
 else
   puts "Drink water and breathe"
