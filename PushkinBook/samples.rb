cb47df2d5be9f0d136b0e6313e43ba8c94da66a2
ABDNdev <igor@abidin.ru>
2021-11-18 15:30:34 +0300

    Add new sample

--- PushkinBook/samples.rb
+++ PushkinBook/samples.rb
@@ -2108,7 +2108,41 @@ x.meth1                   # Это метод 1
 # Но если такое поведение желательно, то его можно реализовать с помощью нехитрого трюка. Существует метод
 # included, который можно переопределить. Он вызывается с параметром - "целевым" классом или модулем 
 # (в который включается данный модуль). 
+module MyMod
 
+  def self.included(klass)
+    def klass.module method 
+      puts "Module (class) method"
+    end
+  end
+
+  def method_1
+    puts "Method 1"
+  end
+
+end
+
+class MyClass
+
+  include MyMod
+
+  def self.class_method 
+    puts "Class method"
+  end
+
+  def method_2
+    puts "Method 2"
+  end
+
+end
+
+x = MyClass.new 
+                             # Выводится:
+MyClass.class_method         # Class method
+x.method_1                   # Method 1
+MyClass.module_method        # Module (class) method
+x.method_2                   # Method 2
+                             
 ------------------------------------------------------------------------------
 # простейшее Rack-приложение на основе класса
 class MyRackApp
