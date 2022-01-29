134f493255b63f39d53fad9d6d185b16b610758a
ABDNdev <igor@abidin.ru>
2022-01-29 14:08:16 +0300

    Add sample

--- PushkinBook/samples.rb
+++ PushkinBook/samples.rb
@@ -4665,6 +4665,44 @@ answer3 = fiber.resume                  # должно быть 8
 # Наконец, библиотека fiber наделяет каждый объект волокна методом экземпляра transfer, который позволяет
 # передавать управление от одного волокна другому (чем достигается по-настоящему кооперативное поведение).
 # Чтобы закрыть эту тему, перепишем пример поиска в нескольких массивах с использованием волокон:
+require 'fiber'
+
+class MaxFiber < Fiber 
+  attr_accessor :result 
+end
+
+def max_by_fiber(interval, collections)
+  fibers = []
+  collections.each_with_index do |numbers|
+    fibers << MaxFiber.new do 
+      me = Fiber.current 
+      me.result = numbers[0]
+      numbers.each_with_index do |n, i|
+        me.result = n if n > me.result
+        Fiber.yield me.result if (i+1) % 3 == 0 
+      end
+      me.result
+    end
+  end
+
+  start_time = Time.now 
+  while fibers.any? &:alive? 
+    break if Time.now - start_time > interval
+    fibers.each { |f| puts f.resume if f.alive? }
+  end
+
+  values = fibers.map &:result
+  values.compact.max || 0
+end
+
+collections = [
+       [1, 25, 3, 7, 42, 64, 55],
+       [3, 77],
+       [3, 33, 7, 44, 77, 102, 92, 10, 11],
+       [1, 2, 3, 4, 5, 6, 7, 8, 9, 77, 2, 3]]
+
+biggest = max_by_fiber(0.5, collections)
+
 
 
 -----------------------------------------------------------------------
